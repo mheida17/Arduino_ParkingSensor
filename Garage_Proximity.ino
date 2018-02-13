23f55f7091304e2fbb373a7d8ae78821353eac83
Matthew Heida <mheida17@gmail.com>
2018-02-12 20:49:23 -0500

    Refactored code to be more readable and modular

--- Garage_Proximity.ino
+++ Garage_Proximity.ino
@@ -3,79 +3,124 @@
 // Proximity Sensor: HC-SR04
 // Arduino Pro Mini 328 - 5V/16MHz
 
-#define trigPin 13
-#define echoPin 12
-#define LED_far 11
-#define LED_med 10
-#define LED_stp 9
+#define trigPin 4
+#define echoPin 3
+#define LED_GREEN 7
+#define LED_YELLOW 8
+#define LED_RED 9
+#define RED 2
+#define YELLOW 1
+#define GREEN 0
+#define TWO_MINUTES 60
+#define QUARTER_SECOND 250
+#define HALF_SECOND 500
+#define FULL_SECOND 1000
+
 long duration, distance, timeElapsed;
-int previousState;
+int previousState, currentState;
 
 void setup() {
+  /** Setup all GPIO pins */
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(LED_far, OUTPUT);
-  pinMode(LED_med, OUTPUT);
-  pinMode(LED_stp, OUTPUT);
-  previousState = 0;
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  testLED();
 }
 
 void loop() {
+  /** Sense distance using HC-SR04 using provided algorithm */
+  distance = senseDistance();
+
+  /** Set state based on distance away from sensor */
+  currentState = checkState();
+
+  /** If the state has changed, increment the timer that controls LED poweroff */
+  timeElapsed = (currentState == previousState) ? (timeElapsed+1) : 0;
+
+  /** Loop to determine if the LED's should be powered off */
+  if (timeElapsed < TWO_MINUTES) {
+    setLEDOn(currentState);
+    delay(500);
+  } else if (timeElapsed >= TWO_MINUTES) {
+    setLEDOff();
+  }
+  
+  previousState = currentState;
+  delay(QUARTER_SECOND);
+}
+
+long senseDistance() {
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = duration / 58;
-  if (distance < 30) {
-    if (previousState == 1) {
-      timeElapsed += 1;
-    } else { 
-      timeElapsed = 1;
-    }
-    previousState = 1;
-    if (timeElapsed < 1000) {
-      digitalWrite(LED_stp, HIGH);
-      digitalWrite(LED_med, LOW);
-      digitalWrite(LED_far, LOW);
-    } else if (timeElapsed == 1000) {
-      digitalWrite(LED_stp, LOW);
-      digitalWrite(LED_med, LOW);
-      digitalWrite(LED_far, LOW);
-    }
-  } else if ((distance > 30) && (distance < 60)) {
-    if (previousState == 2) {
-      timeElapsed += 1;
-    } else { 
-      timeElapsed = 1;
-    }
-    previousState = 2;
-    if (timeElapsed < 1000) {
-      digitalWrite(LED_stp, LOW);
-      digitalWrite(LED_med, HIGH);
-      digitalWrite(LED_far, LOW);
-    } else if (timeElapsed == 1000) {
-      digitalWrite(LED_stp, LOW);
-      digitalWrite(LED_med, LOW);
-      digitalWrite(LED_far, LOW);
-    }
-  } else {
-    if (previousState == 3) {
-      timeElapsed += 1;
-    } else { 
-      timeElapsed = 1;
-    }
-    previousState = 3;
-    if (timeElapsed < 1000) {
-      digitalWrite(LED_stp, LOW);
-      digitalWrite(LED_med, LOW);
-      digitalWrite(LED_far, HIGH);
-    } else if (timeElapsed == 1000) {
-      digitalWrite(LED_stp, LOW);
-      digitalWrite(LED_med, LOW);
-      digitalWrite(LED_far, LOW);
-    }
-  }
-  delay(500);
+
+  return distance;
 }
+
+int checkState(){
+  if (distance < 30) {
+    currentState = RED;
+  }else if (distance < 60) {
+    currentState = YELLOW;
+  }else {
+    currentState = GREEN;
+  }
+
+  return currentState;
+}
+
+void setLEDOn(int currentState){
+  switch (currentState) {
+    case RED:
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      break;
+    case YELLOW:
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      break;
+    case GREEN:
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, HIGH);
+      break;
+    default:
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_YELLOW, HIGH);
+      digitalWrite(LED_GREEN, HIGH);
+  }
+}
+
+void setLEDOff(){
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+}
+
+void testLED(){
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  delay(FULL_SECOND);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+  delay(FULL_SECOND);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  delay(FULL_SECOND);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+}
+
+  
